b3f81fd6b0af3e37b785f5d692012aeffdb8ca55
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-04-08 02:44:56 +0300

    init.py codes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,19 @@
+# app/__init__.py
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    from app.models import db
+    db.init_app(app)
+
+    from app.routes import routes_bp
+    app.register_blueprint(routes_bp)
+
+    return app

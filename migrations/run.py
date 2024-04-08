22dca709caf7a5f5c85abd12495a11f994b96024
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-04-08 07:56:51 +0300

    run.py added

---
+++ migrations/run.py
@@ -0,0 +1,11 @@
+# run.py
+
+import sys
+sys.path.append('/home/wolfy/Moringa/phase-4/pizza-restaurant-flask-api') 
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
